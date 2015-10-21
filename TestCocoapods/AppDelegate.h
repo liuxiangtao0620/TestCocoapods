16e7410fc8f4c6f7e7c6a456f5da8520b5b1be2e
liuxiangtao <liuxiangtao@liuxiangtaos-macbook-pro.local>
2015-10-21 21:27:29 +0800

    Initial Commit

---
+++ TestCocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCocoapods
+//
+//  Created by liuxiangtao on 15/10/21.
+//  Copyright © 2015年 liuxiangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCocoapods
+//
+//  Created by liuxiangtao on 15/10/21.
+//  Copyright © 2015年 liuxiangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
